59bc798487b086e0925ea222a8f45b4e2f635c76
enegrika <sergeigroshev@gmail.com>
2021-03-16 06:09:04 +0700

    Second commit

--- src/main/java/springmavenhelloworld.java
+++ src/main/java/springmavenhelloworld.java
@@ -1,5 +1,6 @@
 public class springmavenhelloworld {
     public static void main(String[] args) {
         System.out.println("Hello everybody");
+        System.out.println("Added 2nd line");
     }
 }
